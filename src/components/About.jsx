d78fa73519a27156c28ed86bb290f43bb38c597b
lakshaymahur24 <118592194+lakshaymahur24@users.noreply.github.com>
2026-04-19 21:12:59 +0530

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,15 +1,21 @@
 export default function About() {
   return (
-    <section className="section grid md:grid-cols-2 gap-10 items-center">
-      <img src="/profile.jpg" className="rounded-xl" />
+    <section className="section grid md:grid-cols-2 gap-12 items-center">
+
+      <img
+        src="/profile.jpg"
+        className="rounded-2xl"
+      />
 
       <div>
-        <h2 className="text-3xl font-bold mb-4">About Me</h2>
-        <p className="text-gray-400">
-          I'm Lakshay Mahur, a Product Designer focused on building scalable
-          digital experiences combining UX and product thinking.
+        <h2 className="text-4xl font-bold mb-4">About Me</h2>
+
+        <p className="text-gray-400 text-lg leading-relaxed">
+          I’m Lakshay Mahur, a Product Designer focused on building
+          scalable digital experiences combining UX and product thinking.
         </p>
       </div>
+
     </section>
   );
 }
